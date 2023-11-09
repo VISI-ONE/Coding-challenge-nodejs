a4e9f7ad50d0872be5954474434c05f376d0f5a1
surazzarus <suraz.zarus@gmail.com>
2023-11-09 12:15:59 +0100

    Add migrations and seed for vehicle entity

---
+++ migrations/20231109111241_vehicles.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('vehicle', (table) => {
+    table.string('name').notNullable();
+    table.integer('tenant_id').unsigned();
+    table.foreign('tenant_id').references('tenant.id').onDelete('CASCADE');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('vehicle');
+};
