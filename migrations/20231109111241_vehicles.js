46632fb0f954cd3fae9a93a838378d2279e9715c
Suraj Shrestha <suraz.zarus@gmail.com>
2023-11-18 14:57:02 +0100

    Add id as primary key for vehicle table and add logs for migration success or error

--- migrations/20231109111241_vehicles.js
+++ migrations/20231109111241_vehicles.js
@@ -4,9 +4,16 @@
  */
 exports.up = function(knex) {
   return knex.schema.createTable('vehicle', (table) => {
+    table.increments('id').primary();
     table.string('name').notNullable();
     table.integer('tenant_id').unsigned();
     table.foreign('tenant_id').references('tenant.id').onDelete('CASCADE');
+  })
+  .then(() => {
+    console.log('Migration completed successfully. Vehicle Table created.')
+  })
+  .catch((error) => {
+    console.error('Error during migration:', error);
   });
 };
 
