cb50e3253604f133989a13066a9e0008294eb233
JoshuaH-sudo <JHwork101@protonmail.com>
2024-02-28 15:26:28 +0100

    refactor to have vehicle id contain as a forgin key in the priceboard table

--- migrations/20231102120100_create_vehicle_table.js
+++ migrations/20231102120100_create_vehicle_table.js
@@ -2,8 +2,6 @@ exports.up = function (knex) {
     return knex.schema.createTable('vehicle', (table) => {
       table.increments('id').primary();
       table.string('name').notNullable();
-      table.decimal('priceboard_id').unsigned();
-      table.foreign('priceboard_id').references('priceboard.id').onDelete('CASCADE');
       table.integer('tenant_id').unsigned();
       table.foreign('tenant_id').references('tenant.id').onDelete('CASCADE');
     });
--- seeds/20231102120000_create_vehicle.js
+++ seeds/20231102120000_create_vehicle.js
@@ -5,10 +5,10 @@ exports.seed = async function (knex) {
   // If there's no existing data, insert the sample data
   if (existingVehicles.length === 0) {
     return knex("vehicle").insert([
-      { name: "Car 1", priceboard_id: 1, tenant_id: 1 },
-      { name: "Car 2", priceboard_id: 2, tenant_id: 1 },
-      { name: "Car 3", priceboard_id: 3, tenant_id: 2 },
-      { name: "Car 4", priceboard_id: 4, tenant_id: 2 },
+      { name: "Car 1", tenant_id: 1 },
+      { name: "Car 2", tenant_id: 1 },
+      { name: "Car 3", tenant_id: 2 },
+      { name: "Car 4", tenant_id: 2 },
     ]);
   }
 };
