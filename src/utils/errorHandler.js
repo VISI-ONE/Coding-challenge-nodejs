d6f25660a8719ee570f345e285572fc6b7ebc58c
jeffascript <ofoaro.jeff@gmail.com>
2024-03-24 17:07:05 +0100

    chore [cleanup] : remove dead code

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,12 +1,3 @@
-// export function throwErrorWithStatus(
-//   message = "An error occurred",
-//   statusCode = 400
-// ) {
-//   let error = new Error(message);
-//   error.statusCode = statusCode;
-//   throw error;
-// }
-
 class CustomError extends Error {
   constructor(message = "An error occurred", statusCode = 400) {
     super(message);
